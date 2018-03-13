b1f2334a7736fd8d2c0f703e48bb396eb6539929
Anastasiya_Sheptur <nasya2611@gmail.com>
2018-03-13 14:19:48 +0200

    Test for reviews

--- RentSG-api/app/models/user.rb
+++ RentSG-api/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :authored_reviews, foreign_key: :user_id, class_name: 'Review', dependent: :destroy
 end
--- RentSG-api/spec/models/review_spec.rb
+++ RentSG-api/spec/models/review_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Review, type: :model do
   # and belongs to a reviewable records
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:reviewable) }
+
+  context 'create review to user and item' do
+    it 'shows reviews from one user to another and some item' do
+      kate = User.create(name: 'Kate', email: 'asd@gmail.com',
+                         phone: '123123', address: 'Lviv')
+      marie = User.create(name: 'Marie', email: 'ad@gmail.com',
+                          phone: '234321', address: 'Kyiv')
+      snowboard = marie.items.create(name: 'Snowboard')
+      rw1 = kate.reviews.create(reviewable: marie, rw_text: 'Nice')
+      rw2 = kate.reviews.create(reviewable: snowboard, rw_text: 'Best board')
+      rw3 = marie.authored_reviews.create(reviewable: kate, rw_text: 'Nice communication')
+      expect(kate.reviews).to eq([rw1, rw2])
+      expect(kate.authored_reviews).to eq([rw3])
+    end
+  end
 end
